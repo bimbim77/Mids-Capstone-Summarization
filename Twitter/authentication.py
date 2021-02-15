e56d783352a58722b8e2a8957ba39d4b19bb05df
Imran Manji <imran.manji2001@berkeledy.edu>
2021-02-14 20:29:14 -0800

    Twitter Updates

--- Twitter/authentication.py
+++ Twitter/authentication.py
@@ -1,22 +1,68 @@
 #Imports
 from twython import Twython
 import requests
+import configparser
+import webbrowser
+import time
 
 #API Credentils
-APP_KEY = "cAELLleclnVvv3x7PqB5tJxrk"
-APP_SECRET = "9X8XzWeXbccs9K8JIdfssq60WsIpJfJrZSxzPlhhDSf7XP49y7"
+def get_app_tokens():
+	config = configparser.ConfigParser()
+	config.read('config.txt')
+	section = 'TWITTER'
 
-#Call Twython Package
-twitter = Twython(APP_KEY, APP_SECRET)
-auth = twitter.get_authentication_tokens()
+	APP_KEY = config.get(section,'APP_KEY')
+	APP_SECRET = config.get(section,'APP_SECRET')
+	return APP_KEY,APP_SECRET
 
-#Store OAuth Tokens
-OAUTH_TOKEN = auth['oauth_token']
-OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
-oauth_verifier_url = auth['auth_url']
-oauth_verifier = requests.get(oauth_verifier_url)
+def get_auth_tokens(APP_KEY,APP_SECRET):
+	twitter = Twython(APP_KEY, APP_SECRET)
+	auth = twitter.get_authentication_tokens()
 
-#Print Output
-print("Verifier URL is:" + oauth_verifier_url)
-print("OAUTH_TOKEN is:" + OAUTH_TOKEN)
-print("OAUTH TOKEN SECRET is:" + OAUTH_TOKEN_SECRET)
+	#Store OAuth Tokens
+	OAUTH_TOKEN = auth['oauth_token']
+	OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+	OAUTH_VERIFIER_URL = auth['auth_url']
+
+	#Open Authentication URL
+	print("The Authorization URL will now open in 5 seconds. After authorizing access, please input the PIN that appears on the screen.")
+	time.sleep(5)
+	webbrowser.open(OAUTH_VERIFIER_URL)
+
+	OAUTH_VERIFIER = input("Enter the PIN value: ") 
+
+	return OAUTH_TOKEN, OAUTH_TOKEN_SECRET, OAUTH_VERIFIER
+
+def get_access_codes(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET,OAUTH_VERIFIER):
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	final_step = twitter.get_authorized_tokens(OAUTH_VERIFIER)
+
+	OAUTH_TOKEN_FINAL = final_step['oauth_token']
+	OAUTH_TOKEN_SECRET_FINAL = final_step['oauth_token_secret']
+
+	return OAUTH_TOKEN_FINAL, OAUTH_TOKEN_SECRET_FINAL, APP_KEY, APP_SECRET
+
+def auth():
+	TW_APP_KEY, TW_APP_SECRET = get_app_tokens()
+	TW_OAUTH_TOKEN, TW_OAUTH_TOKEN_SECRET, TW_OAUTH_VERIFIER = get_auth_tokens(TW_APP_KEY,TW_APP_SECRET)
+	TW_OAUTH_TOKEN_FINAL, TW_OAUTH_TOKEN_SECRET_FINAL, TW_APP_KEY, TW_APP_SECRET = get_access_codes(TW_APP_KEY,TW_APP_SECRET,TW_OAUTH_TOKEN, TW_OAUTH_TOKEN_SECRET, TW_OAUTH_VERIFIER)
+
+	#Update Credentials to Config file
+	config = configparser.ConfigParser()
+	config.read('config.txt')
+	twitter_section = config["TWITTER"]
+
+	#Update the password
+	twitter_section["OAUTH_TOKEN"] = TW_OAUTH_TOKEN_FINAL
+	twitter_section['OAUTH_TOKEN_SECRET'] = TW_OAUTH_TOKEN_SECRET_FINAL
+
+	#Write changes back to file
+	with open('config.txt', 'w') as conf:
+		config.write(conf)
+
+	return TW_OAUTH_TOKEN_FINAL, TW_OAUTH_TOKEN_SECRET_FINAL, TW_APP_KEY, TW_APP_SECRET
+
+
+
+
+
--- Twitter/post.py
+++ Twitter/post.py
@@ -1,24 +1,40 @@
 #Imports
 from twython import Twython
-import requests
+import authentication as a
 import configparser
 
-#API Credentils
+#Tweet inputs
+article_link = 'https://www.nytimes.com/2021/02/09/us/politics/trump-impeachment-trial-senate.html'
+summary = '6 Republicans vote with Democrats to advance trial.'
+tweet_input = summary + ' ' + article_link
+
+#Access Credentials
 config = configparser.ConfigParser()
 config.read('config.txt')
 section = 'TWITTER'
 
 APP_KEY = config.get(section,'APP_KEY')
 APP_SECRET = config.get(section,'APP_SECRET')
-
-OAUTH_TOKEN = config.get(section,'OAUTH_TOKEN')
-OAUTH_TOKEN_SECRET = config.get(section, 'OAUTH_TOKEN_SECRET')
+OAUTH_TOKEN = config.get(section, 'OAUTH_TOKEN')
+OAUTH_TOKEN_SECRET = config.get(section,'OAUTH_TOKEN_SECRET')
 
 #Post to Twitter
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+try:
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	try:
+		twitter.update_status(status=tweet_input)
+		print("Tweet Successful")
+	except:
+		print("Tweet not Successful!")
 
-article_link = 'https://www.nytimes.com/2021/02/09/us/politics/trump-impeachment-trial-senate.html'
-summary = 'Senate agrees to proceed with impeachment trial'
-tweet_input = summary + ' ' + article_link
+except:
+	print("You need to authenticate first!")
+	#Authentication
+	OAUTH_TOKEN, OAUTH_TOKEN_SECRET, APP_KEY, APP_SECRET = a.auth()
 
-twitter.update_status(status=tweet_input)
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	try:
+		twitter.update_status(status=tweet_input)
+		print("Tweet Successful")
+	except:
+		print("Tweet not Successful!")
